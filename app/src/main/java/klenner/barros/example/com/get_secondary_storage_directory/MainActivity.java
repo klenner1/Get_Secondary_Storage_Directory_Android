02a9f7880e7cbd826bed36d243a9b897ba0605c1
klenner1 <klenne.al@gmail.com>
2018-04-16 22:29:53 -0300

    Commit

---
+++ app/src/main/java/klenner/barros/example/com/get_secondary_storage_directory/MainActivity.java
@@ -0,0 +1,13 @@
+package klenner.barros.example.com.get_secondary_storage_directory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

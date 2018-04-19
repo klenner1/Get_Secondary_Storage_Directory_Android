a108fabcf3af2a4dc78db4a66612eaca5e607067
klenner1 <klenne.al@gmail.com>
2018-04-19 14:34:32 -0300

    Directories class

--- app/src/main/java/klenner/barros/example/com/get_secondary_storage_directory/MainActivity.java
+++ app/src/main/java/klenner/barros/example/com/get_secondary_storage_directory/MainActivity.java
@@ -2,12 +2,41 @@ package klenner.barros.example.com.get_secondary_storage_directory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import klenner.barros.example.com.get_secondary_storage_directory.Utils.Directories;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnPrimary,btnSecondary;
+    private TextView txtVwPrimary,txtVwSecondary;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPrimary=findViewById(R.id.btnPrimary);
+        btnSecondary=findViewById(R.id.btnSecondary);
+        txtVwPrimary=findViewById(R.id.txtVwPrimary);
+        txtVwSecondary=findViewById(R.id.txtVwSecondary);
+
+        btnPrimary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String primaryStoreDir =Directories.getPrimaryStoreDir();
+                txtVwPrimary.setText(primaryStoreDir);
+            }
+        });
+
+        btnSecondary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String primaryStoreDir =Directories.getSecondaryStoreDir(getApplicationContext());
+                txtVwSecondary.setText(primaryStoreDir);
+            }
+        });
     }
 }
--- app/src/main/java/klenner/barros/example/com/get_secondary_storage_directory/Utils/Directories.java
+++ app/src/main/java/klenner/barros/example/com/get_secondary_storage_directory/Utils/Directories.java
@@ -1,4 +1,49 @@
 package klenner.barros.example.com.get_secondary_storage_directory.Utils;
 
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
 public class Directories {
+
+    private final static List<String> sdCardPossiblePath = Arrays.asList("external_sd", "ext_sd", "external", "extSdCard", "sdcard2", "media_rw");
+
+    public static String getPrimaryStoreDir() {
+        return Environment.getExternalStorageDirectory().toString();
+    }
+    public static String getSecondaryStoreDir(Context context) {
+        String secStore = null;
+        try {
+            for (String sdPath : sdCardPossiblePath) {
+                File file = new File("/mnt/", sdPath);
+                if (file.exists() && file.isDirectory() && file.canWrite()) {
+                    try {
+                        secStore = file.getAbsolutePath();
+                    }catch (Exception e){
+                        Log.e("",e.getMessage(),e);
+                        secStore=null;
+                    }
+
+                }
+            }
+            if (secStore==null) {
+                File[] externalFilesDirs = context.getExternalFilesDirs(null);
+                if (externalFilesDirs != null && externalFilesDirs.length > 1 && externalFilesDirs[1] != null) {
+                    secStore = externalFilesDirs[1].getAbsolutePath();
+                    secStore=secStore.split("Android")[0];
+                }
+            }
+            if (secStore == null) {
+                secStore = System.getenv("SECONDARY_STORAGE");
+            }
+        }catch (Exception e){
+            Log.e("",e.getMessage(),e);
+        }
+        return secStore;
+    }
+
 }
